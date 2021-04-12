67218eb3840546cee151e1bc00d07e9f6c43ca10
StackDoubleFlow <ojaslandge@gmail.com>
2021-04-12 02:43:25 -0400

    read

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use data::DllData;
 use std::fs::File;
 
 fn main() {
-    let f = File::open("parsed.json").expect("Unable to JSON dump");
+    let f = File::open("parsed.json").expect("Unable to read JSON dump");
     let json: DllData = serde_json::from_reader(f).unwrap();
     dbg!(&json.types[0].this.name);
 }
